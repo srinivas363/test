30ce1024e37bb917e1cc85ffc43c730560e88f24
Ubuntu <ubuntu@ip-172-31-7-161.ap-south-1.compute.internal>
2023-11-24 08:08:49 +0000

    changes

--- 1.sh
+++ 1.sh
@@ -1,22 +1,28 @@
-#!/bin/bash
+set -x
+now=$(date +%s)
+threshold=$(date -d '6 months ago' +%s)
 
-# Get the current date
-current_date=$(date +%Y-%m-%d)
+for k in $(git branch -a | sed s/^..//); do
+    commit_date=$(git log --color=always -1 --pretty=format:%ci $k)
 
-# Calculate the date 3 months ago
-three_months_ago=$(date -d "$current_date - 3 months" +%Y-%m-%d)
+    # Check if commit date is not empty
+    if [ -n "$commit_date" ]; then
+        commit_date_unix=$(date -d"$commit_date" +%s)
 
-# Get all the branches older than 3 months
-old_branches=$(git for-each-ref refs/heads --sort=-committerdate --format='%(refname)' | while read branch; do
-    commit_date=$(git log -1 --pretty=%cd --date=format:%s "$branch")
-    if [[ "$commit_date" < "$three_months_ago" ]]; then
-        echo "$branch"
+        if [ $commit_date_unix -lt $threshold ]; then
+            echo -e $(git log --color=always -1 --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k --)\\t"$k"
+
+            git tag archive/$k $k
+
+            git branch -m "$branch" "$archive_dir/$branch"
+
+        fi
     fi
-done)
+done | sort
 
-# Archive the old branches
-for branch in $old_branches; do
-    git checkout "$branch"
-    git archive --format=tar.gz --output="/path/to/archives/$branch.tar.gz" .
-    git checkout main
-done
+# Remove the original remote branches
+#git remote prune origin
+
+# Push the changes to the remote repository
+#git push origin --tags --force --prune
+
--- 2.sh
+++
@@ -1,7 +0,0 @@
-old_branches=$(git for-each-ref refs/heads --sort=-committerdate --format='%(refname)' | while read branch; do
-    commit_date=$(git log -1 --pretty=%cd --date=format:%s "$branch")
-    if [[ "$commit_date" < "$three_months_ago" ]]; then
-        echo "$branch"
-    fi
-done)
-
